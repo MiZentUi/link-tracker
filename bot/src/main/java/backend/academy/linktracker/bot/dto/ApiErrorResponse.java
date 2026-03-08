d58f6c54cb08c33bc03beb62e86197e1f73d0e4f
Denis Skrobot <mizentui@gmail.com>
2026-03-09 00:38:06 +0300

    feat: updates api

---
+++ bot/src/main/java/backend/academy/linktracker/bot/dto/ApiErrorResponse.java
@@ -0,0 +1,27 @@
+package backend.academy.linktracker.bot.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ApiErrorResponse {
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String code;
+
+    @NotNull
+    private String exceptionName;
+
+    @NotNull
+    private String exceptionMessage;
+    private List<String> stacktrace;
+}
